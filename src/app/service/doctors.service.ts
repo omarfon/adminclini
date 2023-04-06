edfd1932c33bb60d5fdcf50080a1bfe6c7ac1a5b
omar Fonseca <fonsecaom79@gmail.com>
2023-04-06 10:33:30 -0500

    subiendo maestros

--- src/app/service/doctors.service.ts
+++ src/app/service/doctors.service.ts
@@ -1,15 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { urldev } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorsService {
 
-  constructor(public afs: AngularFirestore) { }
+  private url =  `${urldev}/maestros/doctors`;
+
+  constructor(public afs: AngularFirestore,
+              public http: HttpClient) { }
 
   getAllDoctors(){
-    return this.afs.collection('doctors').valueChanges();
+    return this.http.get( this.url);
+    /* return this.afs.collection('doctors').valueChanges(); */
   }
 
   getAllSpecialtyes(){
